7a95b8a7d58ab8cb2e295a00cbef2f09849cced8
Venkat Nallapu <nallapuvenkat6@gmail.com>
2026-02-08 00:36:27 +0530

    Add files via upload

---
+++ src/evaluate.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+import yaml
+import os
+import mlflow
+from urllib.parse import urlparse
+
+
+os.environ['MLFLOW_TRACKING_URI']="https://dagshub.com/krishnaik06/machinelearningpipeline.mlflow"
+os.environ['MLFLOW_TRACKING_USERNAME']="krishnaik06"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ecee2adb4e36a250ae3251f"
+
+
+# Load parameters from params.yaml
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def evaluate(data_path,model_path):
+    data=pd.read_csv(data_path)
+    X = data.drop(columns=["Outcome"])
+    y = data["Outcome"]
+
+    mlflow.set_tracking_uri("https://dagshub.com/krishnaik06/machinelearningpipeline.mlflow")
+
+    ## load the model from the disk
+    model=pickle.load(open(model_path,'rb'))
+
+    predictions=model.predict(X)
+    accuracy=accuracy_score(y,predictions)
+    ## log metrics to MLFLOW
+
+    mlflow.log_metric("accuracy",accuracy)
+    print("Model accuracy:{accuracy}")
+
+if __name__=="__main__":
+    evaluate(params["data"],params["model"])
